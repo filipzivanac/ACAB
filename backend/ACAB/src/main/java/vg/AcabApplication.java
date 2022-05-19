5319fd4bb00672172ecb2f6c98d09a15bc48a5fa
Filip <filip.zivanac@gmail.com>
2022-05-19 22:03:29 +0200

    setup

---
+++ backend/ACAB/src/main/java/vg/AcabApplication.java
@@ -0,0 +1,13 @@
+package vg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcabApplication.class, args);
+	}
+
+}
