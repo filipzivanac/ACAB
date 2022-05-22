c80f2e8862f0f8049fb001ef67b226fb5bb6b7bd
dusanbrkic <dusanbrk@gmail.com>
2022-05-23 01:50:23 +0200

    added lombok

--- backend/ACAB/src/main/java/vg/model/Incident.java
+++ backend/ACAB/src/main/java/vg/model/Incident.java
@@ -1,45 +1,18 @@
 package vg.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Incident {
 	private int cops;
 	private int criminals;
 	private WeaponType weapon;
 	private PoliceResponseType allreadyUsedPoliceResponse;
-	private Boolean threat;
-	
-	public Incident() {
-		super();
-	}
-	public int getCops() {
-		return cops;
-	}
-	public void setCops(int cops) {
-		this.cops = cops;
-	}
-	public int getCriminals() {
-		return criminals;
-	}
-	public void setCriminals(int criminals) {
-		this.criminals = criminals;
-	}
-	public WeaponType getWeapon() {
-		return weapon;
-	}
-	public void setWeapon(WeaponType weapon) {
-		this.weapon = weapon;
-	}
-	public PoliceResponseType getAllreadyUsedPoliceResponse() {
-		return allreadyUsedPoliceResponse;
-	}
-	public void setAllreadyUsedPoliceResponse(PoliceResponseType allreadyUsedPoliceResponse) {
-		this.allreadyUsedPoliceResponse = allreadyUsedPoliceResponse;
-	}
-	public Boolean getThreat() {
-		return threat;
-	}
-	public void setThreat(Boolean threat) {
-		this.threat = threat;
-	}
-	
-	
+	private Boolean suspectThreat;
 }
